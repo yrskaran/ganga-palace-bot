bf778699189e89321b9f990e3e69fb68f370ec05
yrskaran <yrskaran@gmail.com>
2026-09-10 00:42:49 +0530

    app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def ask_ai(sender_id, user_msg):
 
     try:
         completion = groq_client.chat.completions.create(
-            model="llama-3.1-8b-instant",
+            model="llama3-8b-8192",
             messages=payload,
             temperature=0.3,
             max_tokens=150

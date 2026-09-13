d1ec3329216bcd68754895521c7937e5c2a2a1fd
yrskaran <yrskaran@gmail.com>
2026-09-14 00:11:22 +0530

    app.py

--- app.py
+++ app.py
@@ -36,9 +36,9 @@ SHEET_ID = "1E7iI0vSkRlwpiog-GUjN7Gfh35REAhfY_yVG0t63wqY"
 RENDER_EXTERNAL_URL = os.getenv("RENDER_EXTERNAL_URL", "https://ganga-palace-bot.onrender.com")
 
 HOTEL_IMAGES = {
-    "front": "https://images.unsplash.com/photo-1566073771259-6a8506099945?auto=format&fit=crop&w=1200&q=80",
-    "deluxe": "https://images.unsplash.com/photo-1618773928121-c32242e63f39?auto=format&fit=crop&w=1200&q=80",
-    "standard": "https://images.unsplash.com/photo-1590490360182-c33d57733427?auto=format&fit=crop&w=1200&q=80"
+    "front": "images/main hotel.jpg",
+    "deluxe": "images/pexels-artbovich-7722164.jpg",
+    "standard": "images/pexels-kadiravsarr-14750394.jpg"
 }
 
 chat_histories = {}
